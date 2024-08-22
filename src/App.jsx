336b7dd5a72c8ffa1b6d298fa085ab3ae35dc20c
Farhan Abbas <fabbas.ambition@gmail.com>
2024-08-22 02:38:00 -0700

    completed project

---
+++ src/App.jsx
@@ -0,0 +1,132 @@
+import { useEffect, useState } from "react";
+import "bulma/css/bulma.min.css";
+import "./App.css";
+import { generateImageComponent } from "./generateImageComponent.jsx";
+import bodyImagePlaceholder from "./assets/miler-breathable-heathered-jersey-t-shirt.jpg";
+import garmentImagePlaceholder from "./assets/81iB1a1+mWL._AC_UY1000_.jpg";
+
+function App() {
+	const [garmentImage, setGarmentImage] = useState(garmentImagePlaceholder);
+	useEffect(() => {
+		// display the selected image of the user body in the browser.
+		const fileInput = document.getElementById("fileInput");
+		const bodyImage = document.getElementById("bodyImage");
+
+		// Handle file input change event
+		fileInput.addEventListener("change", (event) => {
+			const file = event.target.files[0];
+			if (file) {
+				const reader = new FileReader();
+				reader.onload = (e) => {
+					bodyImage.src = e.target.result;
+				};
+				reader.readAsDataURL(file);
+			}
+		});
+
+		const pasteArea = document.getElementById("pasteArea");
+		if (pasteArea) {
+			pasteArea.onpaste = function (event) {
+				// use event.clipboardData for newer chrome versions
+				var items = (event.clipboardData || event.originalEvent.clipboardData)
+					.items;
+				// find pasted image among pasted items
+				var blob = null;
+				for (var i = 0; i < items.length; i++) {
+					if (items[i].type.indexOf("image") === 0) {
+						blob = items[i].getAsFile();
+					}
+				}
+				// load image if there is a pasted image
+				if (blob !== null) {
+					var reader = new FileReader();
+					reader.onload = function (event) {
+            			setGarmentImage(event.target.result);
+						document.getElementById("pastedImage").src = event.target.result;
+					};
+					reader.readAsDataURL(blob);
+				}
+			};
+		}
+	});
+
+	return (
+		<div
+			className="container is-fluid"
+			style={{ padding: "20px", backgroundColor: "white" }}
+		>
+			<div className="content has-text-centered">
+				<h1 className="title is-1 has-text-primary">
+					<u>TryOnEase</u>
+				</h1>
+				<p className="subtitle is-5 selectImgYourself">Select an Image of Yourself Below</p>
+				<div className="file is-centered mb-4">
+					<label className="file-label">
+						<input
+							className="file-input"
+							type="file"
+							id="fileInput"
+							accept="image/*"
+						/>
+						<span className="file-cta">
+							<span className="file-label">Choose a file…</span>
+						</span>
+					</label>
+				</div>
+				<p className="subtitle is-5">Your Selected Image</p>
+				<figure className="image">
+					<img id="bodyImage" alt="User Body" src={bodyImagePlaceholder} />
+				</figure>
+				<p className="subtitle is-5">Paste a Photo Of a Garment Below</p>
+				<div className="field">
+					<div className="control">
+						<textarea
+							className="textarea"
+							id="pasteArea"
+							placeholder="Paste The Image Of Your Garment Here"
+						></textarea>
+					</div>
+				</div>
+				<p className="subtitle is-5">Your Selected Garment</p>
+				<figure className="image">
+					<img
+						id="pastedImage"
+						alt="Pasted Garment"
+						src={garmentImagePlaceholder}
+					/>
+				</figure>
+				<div className="button-container">
+					<button
+						className="button is-primary is-fullwidth"
+						onClick={() =>
+							generateImageComponent(bodyImagePlaceholder, garmentImage)
+						}
+					>
+						Try It On Yourself!
+					</button>
+				</div>
+				<div className="loading-container" style={{display: "none"}}>
+					<div className="loading"></div>
+					<p className="subtitle is-5 mt-4">
+						Loading...
+					</p>
+				</div>
+				<p className="error-msg subtitle is-5 mt-4" style={{display: "none"}}></p>
+				<div className="generated-image-container" style={{display: "none"}}>
+					<p className="subtitle is-5 mt-4">
+						Here&rsquo;s How You Would Look In This Garment
+					</p>
+					<figure className="image">
+						<img
+							id="resultImage"
+							alt="Generated Image"
+							src=""
+						/>
+					</figure>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/generateImageComponent.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable react/prop-types */
+import { Client } from "https://cdn.jsdelivr.net/npm/@gradio/client@latest/dist/index.js";
+
+// Define the generateImage function in the global scope
+async function generateImage(bodyImagePlaceholder, garmentImg) {
+	try {
+		const response_0 = await fetch(garmentImg);
+		const garmentImgBlob = await response_0.blob();
+	
+		// Initialize the client with the Hugging Face Space
+		const app = await Client.connect("yisol/IDM-VTON");
+
+		// Make a prediction request to the Space
+		const result = await app.predict("/tryon", [
+			{
+				background: await file(
+					bodyImagePlaceholder
+				),
+				layers: [],
+				composite: null,
+			}, // Imageeditor component
+			garmentImgBlob,
+			"", // String in 'parameter_17' Textbox component
+			true, // Boolean in 'Yes' Checkbox component
+			true, // Boolean in 'Yes' Checkbox component
+			30, // Number in 'Denoising Steps' Number component (updated to meet minimum value constraint)
+			42, // Number in 'Seed' Number component (updated to meet minimum value constraint)
+		]);
+
+		// Log the result
+		console.log("success");
+		console.log(result.data);
+		return ["success", result.data[0]["url"]];
+	} catch (error) {
+		// Log detailed error information
+		console.error("Prediction request failed:", error);
+		return ["failure", error];
+	}
+}
+
+// Define the file function
+async function file(url) {
+	const response = await fetch(url);
+	return response.blob();
+}
+
+export function generateImageComponent(bodyImagePlaceholder, garmentImg) {
+    let response = "";
+    const handleGenerateImage = async () => {
+		let loader = document.getElementsByClassName("loading-container")[0];
+
+		loader.style.display = "flex";
+		window.scrollTo(0, document.body.scrollHeight);
+
+		response = await generateImage(bodyImagePlaceholder, garmentImg);
+		// response = ["success", "https://www.w3schools.com/w3images/lights.jpg"];
+		console.log("finished", response);
+
+		loader.style.display = "none";
+
+		if (response[0] === "success") {
+			let resultImg = document.getElementById("resultImage");
+			resultImg.src = response[1];
+			let generatedImgDiv = document.getElementsByClassName("generated-image-container")[0];
+			generatedImgDiv.style.display = "block";
+			let errorMsg = document.getElementsByClassName("error-msg")[0];
+			errorMsg.style.display = "none";
+			window.scrollTo(0, document.body.scrollHeight);
+		}
+		else {
+			let errorMsg = document.getElementsByClassName("error-msg")[0];
+			errorMsg.style.display = "block";
+			errorMsg.innerHTML = "Error: " + response[1]["message"];
+			window.scrollTo(0, document.body.scrollHeight);
+		}
+    };
+    handleGenerateImage();
+}
